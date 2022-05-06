ba3abee1a387e29b04fb85fbe01ca151fe03900d
Samuel CASAS DRAY <samuelcasasdray@gmail.com>
2022-05-06 16:14:01 +0200

    m

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as jose from 'jose';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  data: any;
+
+  constructor() {
+    let key =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SARsBE5x_ua2ye823r2zKpQNaew3Daq8riKz5A4h3o4";
+    let secret = "your-secret";
+    jose.jwtVerify(key, new TextEncoder().encode(secret)).then((data) => {
+      this.data = data;
+    });
+  }
 }
